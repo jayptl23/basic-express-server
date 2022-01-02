6d69dcae012bf8fc3652e74443718f706639f80e
Jay Patel <29896894+jayptl23@users.noreply.github.com>
2022-01-02 13:07:31 -0500

    configure environment variables, install mongodb

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
 app.use(express.static('client'))
 
 app.get('/hello', (req, res) => res.json('Hello World!'))
 
-const PORT = 3000
+const PORT = process.env.PORT || 4000
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
